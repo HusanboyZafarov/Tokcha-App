1795e030ead8af54507166184add0a5e8ec667af
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-01-09 12:43:00 +0500

    Ending hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 let loader = document.querySelector(".loader"),
-    header_circle = document.querySelector(".header_circle")
+    header_circle = document.querySelector(".header_circle"),
+    hero_circle = document.querySelector(".hero_circle")
 window.addEventListener("load", () => {
     loader.classList.remove("not_loaded")
     header_circle.classList.toggle("moved")
+    hero_circle.classList.toggle("moved")
 })
